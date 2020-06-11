882894e2abd2930869b6dcf717ae8cfebe29b2b1
Sunho Kim <ksunhokim123@naver.com>
2020-06-12 03:06:15 +0900

    Check only on changed files

---
+++ .github/format-check.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+for f in $(git diff --name-only origin/$GITHUB_BASE_REF | grep -E "(vita3k|tools/gen-modules|tools/native-tool)/.*(.cpp|.h)" ); do
+    if [ -f "$f" ]; then
+        if [ "$(diff -u <(cat $f) <(clang-format $f))" != "" ]
+        then
+            echo "run format"
+            exit 1
+        fi
+    fi
+done
