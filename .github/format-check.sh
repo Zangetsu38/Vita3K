e06b2dc5a613b97f5547fa9ff2479afc3630fd91
Sunho Kim <ksunhokim123@naver.com>
2020-06-12 03:10:23 +0900

    Asdf

--- .github/format-check.sh
+++ .github/format-check.sh
@@ -2,12 +2,16 @@
 
 set -ex
 
-for f in $(git diff --name-only origin/$GITHUB_BASE_REF | grep -E "(vita3k|tools/gen-modules|tools/native-tool)/.*(.cpp|.h)" ); do
-    if [ -f "$f" ]; then
-        if [ "$(diff -u <(cat $f) <(clang-format $f))" != "" ]
-        then
-            echo "run format"
-            exit 1
+echo "$GITHUB_EVENT_NAME"
+
+if [ "$GITHUB_EVENT_NAME" == "pull_request" ] then 
+    for f in $(git diff --name-only origin/$GITHUB_BASE_REF | grep -E "(vita3k|tools/gen-modules|tools/native-tool)/.*(.cpp|.h)" ); do
+        if [ -f "$f" ]; then
+            if [ "$(diff -u <(cat $f) <(clang-format $f))" != "" ]
+            then
+                echo "run format"
+                exit 1
+            fi
         fi
-    fi
-done
+    done
+fi
