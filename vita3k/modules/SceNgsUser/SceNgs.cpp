d2a29a1adcde07049a66f5ce593ca93db5ec5c95
Zangetsu38 <dante38@free.fr>
2025-12-29 21:40:08 +0100

    ngs: Fix crash of keyoff for atrac9.

--- vita3k/modules/SceNgsUser/SceNgs.cpp
+++ vita3k/modules/SceNgsUser/SceNgs.cpp
@@ -813,13 +813,18 @@ EXPORT(SceInt32, sceNgsVoiceKeyOff, ngs::Voice *voice) {
     }
 
     voice->is_keyed_off = true;
-    voice->rack->system->voice_scheduler.off(emuenv.mem, voice);
 
-    // call the finish callback, I got no idea what the module id should be in this case
-    voice->invoke_callback(emuenv.kernel, emuenv.mem, thread_id, voice->finished_callback, voice->finished_callback_user_data, 0);
+    for (auto &module : voice->rack->modules) {
+        if (module && module->module_id() == 0x5CE6) {
+            // call the finish callback, I got no idea what the module id should be in this case
+            voice->rack->system->voice_scheduler.off(emuenv.mem, voice);
+            voice->invoke_callback(emuenv.kernel, emuenv.mem, thread_id, voice->finished_callback, voice->finished_callback_user_data, 0);
+            voice->is_keyed_off = false;
+            voice->rack->system->voice_scheduler.stop(emuenv.mem, voice);
 
-    voice->is_keyed_off = false;
-    voice->rack->system->voice_scheduler.stop(emuenv.mem, voice);
+            break;
+        }
+    }
     return SCE_NGS_OK;
 }
 
